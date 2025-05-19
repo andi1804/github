fcb2af0dff8a618ce7b13d77c6212c609de8bc05
andi1804 <162776179+andi1804@users.noreply.github.com>
2025-05-19 23:21:10 +0700

    Add files via upload

---
+++ klinik.sql
@@ -0,0 +1,197 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 19 Bulan Mei 2025 pada 18.20
+-- Versi server: 10.4.28-MariaDB
+-- Versi PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `klinik`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_antrian`
+--
+
+CREATE TABLE `tb_antrian` (
+  `no_antrian` varchar(3) NOT NULL,
+  `no_rm` int(11) NOT NULL,
+  `nama` varchar(25) NOT NULL,
+  `tanggal_periksa` date NOT NULL,
+  `keluhan` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_dokter`
+--
+
+CREATE TABLE `tb_dokter` (
+  `id_dokter` varchar(7) NOT NULL,
+  `nama_dokter` varchar(25) NOT NULL,
+  `alamat_dokter` varchar(40) NOT NULL,
+  `telepon_dokter` varchar(12) NOT NULL,
+  `username` varchar(10) NOT NULL,
+  `password` varchar(10) NOT NULL,
+  `jadwal_praktek` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data untuk tabel `tb_dokter`
+--
+
+INSERT INTO `tb_dokter` (`id_dokter`, `nama_dokter`, `alamat_dokter`, `telepon_dokter`, `username`, `password`, `jadwal_praktek`) VALUES
+('0318002', 'Paramintha', 'Jalan Raya Bogor', '085256534356', 'dokter', 'qwe', 'Sabut 08.00-18.00'),
+('0318004', 'yogi', 'sukabumi', '081210261202', 'yogi', 'qwe', 'Senin-rabu '),
+('0318005', 'Bisma', 'sukabumi', '085256534356', 'bisma', 'qwe', 'Senin-Jumat 08-00-15.00');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_pasien`
+--
+
+CREATE TABLE `tb_pasien` (
+  `no_rm` int(11) NOT NULL,
+  `nama_pasien` varchar(25) NOT NULL,
+  `tanggal_lahir` date NOT NULL,
+  `jenis_kelamin` varchar(1) NOT NULL,
+  `gol_darah` varchar(2) NOT NULL,
+  `pekerjaan_pasien` varchar(15) NOT NULL,
+  `alamat_pasien` varchar(40) NOT NULL,
+  `telepon` varchar(12) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data untuk tabel `tb_pasien`
+--
+
+INSERT INTO `tb_pasien` (`no_rm`, `nama_pasien`, `tanggal_lahir`, `jenis_kelamin`, `gol_darah`, `pekerjaan_pasien`, `alamat_pasien`, `telepon`) VALUES
+(1, 'b', '1999-05-10', 'L', 'A', 'mhs', 'smi', '12345678');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_pegawai`
+--
+
+CREATE TABLE `tb_pegawai` (
+  `id_pegawai` varchar(7) NOT NULL,
+  `nama_pegawai` varchar(25) NOT NULL,
+  `alamat_pegawai` varchar(40) NOT NULL,
+  `telepon_pegawai` varchar(12) NOT NULL,
+  `username` varchar(10) NOT NULL,
+  `password` varchar(10) NOT NULL,
+  `jabatan` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data untuk tabel `tb_pegawai`
+--
+
+INSERT INTO `tb_pegawai` (`id_pegawai`, `nama_pegawai`, `alamat_pegawai`, `telepon_pegawai`, `username`, `password`, `jabatan`) VALUES
+('0118001', 'M rajbi', 'Tebet Barat', '082110290933', 'rajbi', 'qwe', 'Resepsionis'),
+('0118002', 'M Ruslan', 'sukabumi', '081210261292', 'admin', 'qwe', 'Admin'),
+('0118005', 'Iskandar', 'sukabumi', '085256534353', 'iskandar', 'qwe', 'Resepsionis');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_riwayat`
+--
+
+CREATE TABLE `tb_riwayat` (
+  `no_periksa` varchar(9) NOT NULL,
+  `no_rm` int(3) NOT NULL,
+  `tanggal_periksa` date NOT NULL,
+  `tindakan` varchar(500) NOT NULL,
+  `diagnosa` varchar(500) NOT NULL,
+  `resep` varchar(500) NOT NULL,
+  `tekanan_darah` varchar(7) NOT NULL,
+  `keluhan` varchar(500) NOT NULL,
+  `nama_dokter` varchar(32) NOT NULL,
+  `berat` int(3) NOT NULL,
+  `tinggi` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `tb_antrian`
+--
+ALTER TABLE `tb_antrian`
+  ADD PRIMARY KEY (`no_antrian`),
+  ADD KEY `fk_antri_pasien` (`no_rm`);
+
+--
+-- Indeks untuk tabel `tb_dokter`
+--
+ALTER TABLE `tb_dokter`
+  ADD PRIMARY KEY (`id_dokter`);
+
+--
+-- Indeks untuk tabel `tb_pasien`
+--
+ALTER TABLE `tb_pasien`
+  ADD PRIMARY KEY (`no_rm`);
+
+--
+-- Indeks untuk tabel `tb_pegawai`
+--
+ALTER TABLE `tb_pegawai`
+  ADD PRIMARY KEY (`id_pegawai`);
+
+--
+-- Indeks untuk tabel `tb_riwayat`
+--
+ALTER TABLE `tb_riwayat`
+  ADD PRIMARY KEY (`no_periksa`),
+  ADD KEY `fk_rm_dokter` (`no_rm`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_pasien`
+--
+ALTER TABLE `tb_pasien`
+  MODIFY `no_rm` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `tb_antrian`
+--
+ALTER TABLE `tb_antrian`
+  ADD CONSTRAINT `fk_antri_pasien` FOREIGN KEY (`no_rm`) REFERENCES `tb_pasien` (`no_rm`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `tb_riwayat`
+--
+ALTER TABLE `tb_riwayat`
+  ADD CONSTRAINT `fk_rm_dokter` FOREIGN KEY (`no_rm`) REFERENCES `tb_pasien` (`no_rm`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
